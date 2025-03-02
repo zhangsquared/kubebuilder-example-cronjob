7c46dc5db7eef04249d4e62c2fa3e49068ed53dc
Zhang Zhang <hi.zhangzhang@gmail.com>
2025-03-02 16:07:35 -0500

    change tab to spaces

--- api/v1/cronjob_types.go
+++ api/v1/cronjob_types.go
@@ -17,9 +17,9 @@ limitations under the License.
 package v1
 
 import (
-	batchv1 "k8s.io/api/batch/v1"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    batchv1 "k8s.io/api/batch/v1"
+    corev1 "k8s.io/api/core/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -27,47 +27,47 @@ import (
 
 // CronJobSpec defines the desired state of CronJob.
 type CronJobSpec struct {
-	// +kubebuilder:validation:MinLength=0
+    // +kubebuilder:validation:MinLength=0
 
-	// The schedule in Cron format, see https://en.wikipedia.org/wiki/Cron.
-	Schedule string `json:"schedule"`
+    // The schedule in Cron format, see https://en.wikipedia.org/wiki/Cron.
+    Schedule string `json:"schedule"`
 
-	// +kubebuilder:validation:Minimum=0
+    // +kubebuilder:validation:Minimum=0
 
-	// Optional deadline in seconds for starting the job if it misses scheduled time for any reason.
-	// Missed jobs executions will be counted as failed ones.
-	// +optional
-	StartingDeadlineSeconds *int64 `json:"startDeadlineSeconds,omitempty"`
+    // Optional deadline in seconds for starting the job if it misses scheduled time for any reason.
+    // Missed jobs executions will be counted as failed ones.
+    // +optional
+    StartingDeadlineSeconds *int64 `json:"startDeadlineSeconds,omitempty"`
 
-	// Specifies how to treat concurrent executions of a Job.
-	// Valid values are:
-	// - "Allow" (default): allows CronJobs to run concurrently;
-	// - "Forbid": forbids concurrent runs, skipping next run if previous hasn't finished yet;
-	// - "Replace": cancels currently running job and replaces it with a new one
-	// +optional
-	ConcurrencyPolicy ConcurrencyPolicy `json:"concurrencyPolicy,omitempty"`
+    // Specifies how to treat concurrent executions of a Job.
+    // Valid values are:
+    // - "Allow" (default): allows CronJobs to run concurrently;
+    // - "Forbid": forbids concurrent runs, skipping next run if previous hasn't finished yet;
+    // - "Replace": cancels currently running job and replaces it with a new one
+    // +optional
+    ConcurrencyPolicy ConcurrencyPolicy `json:"concurrencyPolicy,omitempty"`
 
-	// This flag tells the controller to suspend subsequent executions, it does not apply to already started executions
-	// Defaults to false.
-	// +optional
-	Suspend *bool `json:"suspend,omitempty"`
+    // This flag tells the controller to suspend subsequent executions, it does not apply to already started executions
+    // Defaults to false.
+    // +optional
+    Suspend *bool `json:"suspend,omitempty"`
 
-	// Specifies the job that will be created when executing a CronJob.
-	JobTemplate batchv1.JobTemplateSpec `json:"jobTemplate"`
+    // Specifies the job that will be created when executing a CronJob.
+    JobTemplate batchv1.JobTemplateSpec `json:"jobTemplate"`
 
-	// +kubebuilder:validation:Minimum=0
+    // +kubebuilder:validation:Minimum=0
 
-	// The number of successful finished jobs to retain.
-	// This is a pointer to distinguish between explicit zero and not specified.
-	// +optional
-	SuccessfulJobsHistoryLimit *int32 `json:"successfulJobsHistoryLimit,omitempty"`
+    // The number of successful finished jobs to retain.
+    // This is a pointer to distinguish between explicit zero and not specified.
+    // +optional
+    SuccessfulJobsHistoryLimit *int32 `json:"successfulJobsHistoryLimit,omitempty"`
 
-	// +kubebuilder:validation:Minimum=0
+    // +kubebuilder:validation:Minimum=0
 
-	// The number of failed finished jobs to retain.
-	// This is a pointer to distinguish between explicit zero and not specified.
-	// +optional
-	FailedJobsHistoryLimit *int32 `json:"failedJobsHistoryLimit,omitempty"`
+    // The number of failed finished jobs to retain.
+    // This is a pointer to distinguish between explicit zero and not specified.
+    // +optional
+    FailedJobsHistoryLimit *int32 `json:"failedJobsHistoryLimit,omitempty"`
 }
 
 // ConcurrencyPolicy describes how the job will be handled.
@@ -77,28 +77,28 @@ type CronJobSpec struct {
 type ConcurrencyPolicy string
 
 const (
-	// AllowConcurrent allows CronJobs to run concurrently.
-	AllowConcurrent ConcurrencyPolicy = "Allow"
+    // AllowConcurrent allows CronJobs to run concurrently.
+    AllowConcurrent ConcurrencyPolicy = "Allow"
 
-	// ForbidConcurrent forbids concurrent runs, skipping next run if previous hasn't finished yet.
-	ForbidConcurrent ConcurrencyPolicy = "Forbid"
+    // ForbidConcurrent forbids concurrent runs, skipping next run if previous hasn't finished yet.
+    ForbidConcurrent ConcurrencyPolicy = "Forbid"
 
-	// ReplaceConcurrent cancels currently running job and replaces it with a new one.
-	ReplaceConcurrent ConcurrencyPolicy = "Replace"
+    // ReplaceConcurrent cancels currently running job and replaces it with a new one.
+    ReplaceConcurrent ConcurrencyPolicy = "Replace"
 )
 
 // CronJobStatus defines the observed state of CronJob.
 type CronJobStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+    // INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+    // Important: Run "make" to regenerate code after modifying this file
 
-	// A list of pointers to currently running jobs.
-	// +optional
-	Active []corev1.ObjectReference `json:"active,omitempty"`
+    // A list of pointers to currently running jobs.
+    // +optional
+    Active []corev1.ObjectReference `json:"active,omitempty"`
 
-	// Information when was the last time the job was successfully scheduled.
-	// +optional
-	LastScheduleTime *metav1.Time `json:"lastScheduleTime,omitempty"`
+    // Information when was the last time the job was successfully scheduled.
+    // +optional
+    LastScheduleTime *metav1.Time `json:"lastScheduleTime,omitempty"`
 }
 
 // +kubebuilder:object:root=true
@@ -106,22 +106,22 @@ type CronJobStatus struct {
 
 // CronJob is the Schema for the cronjobs API.
 type CronJob struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
+    metav1.TypeMeta   `json:",inline"`
+    metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   CronJobSpec   `json:"spec,omitempty"`
-	Status CronJobStatus `json:"status,omitempty"`
+    Spec   CronJobSpec   `json:"spec,omitempty"`
+    Status CronJobStatus `json:"status,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 
 // CronJobList contains a list of CronJob.
 type CronJobList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []CronJob `json:"items"`
+    metav1.TypeMeta `json:",inline"`
+    metav1.ListMeta `json:"metadata,omitempty"`
+    Items           []CronJob `json:"items"`
 }
 
 func init() {
-	SchemeBuilder.Register(&CronJob{}, &CronJobList{})
+    SchemeBuilder.Register(&CronJob{}, &CronJobList{})
 }
--- internal/controller/cronjob_controller_test.go
+++ internal/controller/cronjob_controller_test.go
@@ -17,68 +17,68 @@ limitations under the License.
 package controller
 
 import (
-	"context"
+    "context"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+    . "github.com/onsi/ginkgo/v2"
+    . "github.com/onsi/gomega"
+    "k8s.io/apimachinery/pkg/api/errors"
+    "k8s.io/apimachinery/pkg/types"
+    "sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	batchv1 "tutorial.kubebuilder.io/project/api/v1"
+    batchv1 "tutorial.kubebuilder.io/project/api/v1"
 )
 
 var _ = Describe("CronJob Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
+    Context("When reconciling a resource", func() {
+        const resourceName = "test-resource"
 
-		ctx := context.Background()
+        ctx := context.Background()
 
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
-		}
-		cronjob := &batchv1.CronJob{}
+        typeNamespacedName := types.NamespacedName{
+            Name:      resourceName,
+            Namespace: "default", // TODO(user):Modify as needed
+        }
+        cronjob := &batchv1.CronJob{}
 
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind CronJob")
-			err := k8sClient.Get(ctx, typeNamespacedName, cronjob)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &batchv1.CronJob{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
-					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
-		})
+        BeforeEach(func() {
+            By("creating the custom resource for the Kind CronJob")
+            err := k8sClient.Get(ctx, typeNamespacedName, cronjob)
+            if err != nil && errors.IsNotFound(err) {
+                resource := &batchv1.CronJob{
+                    ObjectMeta: metav1.ObjectMeta{
+                        Name:      resourceName,
+                        Namespace: "default",
+                    },
+                    // TODO(user): Specify other spec details if needed.
+                }
+                Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+            }
+        })
 
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &batchv1.CronJob{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
+        AfterEach(func() {
+            // TODO(user): Cleanup logic after each test, like removing the resource instance.
+            resource := &batchv1.CronJob{}
+            err := k8sClient.Get(ctx, typeNamespacedName, resource)
+            Expect(err).NotTo(HaveOccurred())
 
-			By("Cleanup the specific resource instance CronJob")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &CronJobReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
-			}
+            By("Cleanup the specific resource instance CronJob")
+            Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+        })
+        It("should successfully reconcile the resource", func() {
+            By("Reconciling the created resource")
+            controllerReconciler := &CronJobReconciler{
+                Client: k8sClient,
+                Scheme: k8sClient.Scheme(),
+            }
 
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
-			})
-			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
-		})
-	})
+            _, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+                NamespacedName: typeNamespacedName,
+            })
+            Expect(err).NotTo(HaveOccurred())
+            // TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+            // Example: If you expect a certain status condition after reconciliation, verify it here.
+        })
+    })
 })
